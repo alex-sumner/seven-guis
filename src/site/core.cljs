3375cc1bc39b0d0c676bdfe16d7c94429b8af85a
Alex Sumner <alex_sumner@mentor.com>
2020-12-02 15:11:32 +0000

    added a logo and some formatting

--- src/site/core.cljs
+++ src/site/core.cljs
@@ -1,9 +1,11 @@
 (ns site.core
-  (:require [reagent.dom :as r]))
+  (:require [reagent.dom :as r]
+            [site.components.header :refer [header]]))
 
 (defn app
   []
-  [:div "Seven GUI Tasks"]
+  [:div
+   [header]]
   )
 
 (defn ^:dev/after-load start []
